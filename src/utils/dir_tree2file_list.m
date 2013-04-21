6d56430722b15da94b108acc2199d1820c24af5c
Laz <lkarydas@gmail.com>
2013-04-21 11:57:37 -0400

    Fixed to automatically search all files with .jpg or .png extension

--- src/utils/dir_tree2file_list.m
+++ src/utils/dir_tree2file_list.m
@@ -1,23 +1,22 @@
-function file_list = dir_tree2file_list(file_struct, file_list, ext, readEXIF)
+function file_list = dir_tree2file_list(file_struct, file_list)
 for i = 1:length(file_struct)
     if ~strcmp('.',file_struct(i).name) && ... 
        ~strcmp('..',file_struct(i).name)
         [dum,dum,extension] = fileparts(file_struct(i).name); %#ok<ASGLU>
         clear dum;
-        if strcmpi(extension,ext)
+        if strcmpi(extension,'.jpg') || strcmpi(extension,'.png')
             tmp.name = file_struct(i).name; 
             tmp.rel_path = file_struct(i).rel_path;
             tmp.abs_path = [pwd filesep file_struct(i).rel_path];
             tmp.datenum = file_struct(i).datenum;
             %display(tmp.name)
-            if readEXIF
-                tmp.info = imfinfo([tmp.abs_path filesep tmp.name]);
-            end
+            % Read metadata information
+            tmp.info = imfinfo([tmp.abs_path filesep tmp.name]);
             file_list = [file_list tmp]; %#ok<AGROW>
         end
         if file_struct(i).isdir
             file_list = ...
-                dir_tree2file_list(file_struct(i).files, file_list, ext, readEXIF);
+                dir_tree2file_list(file_struct(i).files, file_list);
         end
     end
 end
--- src/utils/get_file_list.m
+++ src/utils/get_file_list.m
@@ -1,4 +1,4 @@
-function files = get_file_list(dataset_dir, ext, readEXIF, verbose)
+function files = get_file_list(dataset_dir, verbose)
 %get_file_list Recursively read the list of files inside a directory
 %   dataset_dir is the starting directory, ext is the extension of files
 %   you want to get, readEXIF is a boolean flag for whether to read EXIF
@@ -8,21 +8,13 @@ if ~exist('verbose', 'var')
     verbose = true;
 end
 
-if ~exist('readEXIF', 'var')
-    readEXIF = true;
-end
-
-if ~exist('ext', 'var')
-    ext = '.jpg';
-end
-
 if verbose
     fprintf('Getting list of files...')
 end
 % Parse all directories recursively
 file_struct = rec_dir_parser(dataset_dir);
 % Convert the directory tree to a file list
-files = dir_tree2file_list(file_struct, [], ext, readEXIF);
+files = dir_tree2file_list(file_struct, []);
 if verbose
     disp('Done.')
     disp(['Total files: ' num2str(length(files))])
