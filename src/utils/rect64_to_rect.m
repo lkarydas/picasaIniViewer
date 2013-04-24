b65a323dad01c736b5ba66dcb370a699aadef0c6
Laz <lkarydas@gmail.com>
2013-04-24 11:44:03 -0400

    Cleaned up the code, added comments.

--- src/utils/rect64_to_rect.m
+++ src/utils/rect64_to_rect.m
@@ -1,16 +1,20 @@
 function rect = rect64_to_rect(hex_pos, img_size)
+% rect64_to_rect
+% Decodes a Picasa encoded hash string to a [x y w h] rectangle.
 
 while length(hex_pos) < 16
-    hex_pos = ['0' hex_pos];
+    hex_pos = ['0' hex_pos]; %#ok<AGROW>
 end
 
+% Hex to decimal
 f1 = hex2dec(hex_pos(1:4));
 f2 = hex2dec(hex_pos(5:8));
 f3 = hex2dec(hex_pos(9:12));
 f4 = hex2dec(hex_pos(13:16));
-
+% Get image size
 w = img_size(1);
 h = img_size(2);
+% Multiply coordinates by image dimensions
 face_pos = [w*f1 h*f2 w*f3 h*f4];
 face_pos = face_pos/65535;
 w = face_pos(3) - face_pos(1);
